8ee568530bb098f916b4cd66f78b9d1a62b06944
mabyoungg <mabyoungg@gmail.com>
2024-03-02 19:48:28 +0900

    feat: controller 실행

--- src/main/java/com/example/springbootbatch/batch/BatchController.java
+++ src/main/java/com/example/springbootbatch/batch/BatchController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.LocalDateTime;
+
 @Controller
 @RequestMapping("/batch")
 @RequiredArgsConstructor
@@ -18,5 +20,30 @@ public class BatchController {
         batchService.runHelloJob();
 
         return "helloJob OK";
+    }
+
+    @GetMapping("/makeProductLog")
+    @ResponseBody
+    public String runMakeProductLogJob() {
+        LocalDateTime startDate = LocalDateTime
+                .now()
+                .withHour(0)
+                .withMinute(0)
+                .withSecond(0)
+                .withNano(0);
+
+        LocalDateTime endDate = LocalDateTime
+                .now()
+                .withHour(23)
+                .withMinute(59)
+                .withSecond(59)
+                .withNano(999999);
+
+        batchService.runMakeProductLogJob(
+                startDate,
+                endDate
+        );
+
+        return "makeProductLog OK";
     }
 }
--- src/main/java/com/example/springbootbatch/batch/BatchService.java
+++ src/main/java/com/example/springbootbatch/batch/BatchService.java
@@ -7,17 +7,35 @@ import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class BatchService {
     private final JobLauncher jobLauncher;
     private final Job helloJob;
+    private final Job makeProductLogJob;
 
     public void runHelloJob() {
         try {
             JobParameters jobParameters = new JobParametersBuilder()
                     .toJobParameters();
             jobLauncher.run(helloJob, jobParameters);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void runMakeProductLogJob(LocalDateTime startDate_, LocalDateTime endDate_) {
+        try {
+            String startDate = startDate_.toString().substring(0, 10) + " 00:00:00.000000";
+            String endDate = endDate_.toString().substring(0, 10) + " 23:59:59.999999";
+
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addString("startDate", startDate)
+                    .addString("endDate", endDate)
+                    .toJobParameters();
+            jobLauncher.run(makeProductLogJob, jobParameters);
         } catch (Exception e) {
             e.printStackTrace();
         }
